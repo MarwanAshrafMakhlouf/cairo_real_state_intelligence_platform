ca7579bf2816535ad07fceb82d221f75268b616f
MarwanAshrafMakhlouf <marwanashraf593@gmail.com>
2026-05-26 01:49:39 +0300

    UPDATE: EDA

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from src import scraper
 from src import scrape_links
+from src import cleaning
 def main():
     print("Hello from cairo-real-state-intelligence-platform!")
     # scraper.main()
     scraper.main()
+    cleaning.main()
 
 
 if __name__ == "__main__":
